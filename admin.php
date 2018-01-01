26bf5526d193eee72f170351e95419ded48c99fe
kdogan <kamuran1905@yahoo.de>
2018-01-01 23:04:33 +0100

    admin site

--- admin.php
+++ admin.php
@@ -5,6 +5,7 @@ include ("php_script.php");
 ?>
 
 <?php
+$administration = new administration();
 $functions = new functions();
 $functions->getSiteHead();
 
@@ -28,34 +29,17 @@ function checkCookie() {
 <div class="header_second_column" style="background-color:yellow;color:black"><h4> Aministration Bereich</h4></div>
 <div class="header_third_column">
   <form class="search_form">
-  <div style="float:right"><img style="width:50px; float:right;" src="img/adminLogginImg.png" href="#" onclick="closeAdminSite()"></img></div>
-   <!--<div style="float:right"> <input type="text" id="search" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name"></div>-->
+  <div style="float:right"><img style=" width:50px; float:right;" src="img/adminLogginImg.png" href="#" onclick="closeAdminSite()"></img></div>
+  <div style="float:right"><img style=" height:60px;float:right;" src="img/home.png" href="#" onclick="showUsers()"></img></div>
   </form>
 </div>
 </header>
   
-<div class="content">
-  <menu>
-  <li style="width:fil-content">
-      <div class="user_div" id="admin_users">
-        <div class="user_img" style="background-image: url(\'img/users.png\');" href="#" onclick="showUsers()"></div>
-        <p>Employees</p>
-      </div>           
-    </li>
-    <li style="width:fil-content">
-      <div class="user_div" id="admin_users">
-        <div class="user_img" style="background-image: url(\'img/products.png\');" href="#" onclick="showUsers()"></div>
-        <p>Products</p>
-      </div>    
-    </li>
-    <li style="width:fil-content">
-      <div class="user_div" id="admin_users">
-        <div class="user_img" style="background-image: url(\'img/setting.png\');" href="#" onclick="showUsers()"></div>
-        <p>Setting</p>
-      </div>    
-    </li>
-  </menu>
-</div>';
+<div class="content">';
+  
+  $administration->getFirstContentOfAdminSite();
+  
+echo '</div>';
 $functions->getFooter();
 
 ?>
