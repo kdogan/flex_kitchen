c55af7e4150bf0ebf9beac1411d152a77d57ab6c
kdogan <kamuran1905@yahoo.de>
2018-01-11 12:55:47 +0100

    Add user as listview in admin page

--- php_scripts/adminPageContent.php
+++ php_scripts/adminPageContent.php
@@ -15,22 +15,41 @@ function getUserDivsInAdminPage(){
 	require("../php_script.php");
 	$functions = new functions();
 
-        $persons = $functions->getAllFromTable("person");
-        $result = "<h2>No User found... :'(</h2>";
-        if($persons->num_rows >0){
-            $result = "";
-            while($row = $persons->fetch_assoc()){
-                //pass user if it is admin
-                if($row["is_admin"] == "1") continue;
-                $user = $row["firstname"].' '.$row["lastname"];
-                $id = $row["id"];
-                $result = $result.'<li class="user_div" id="'.$id.'">
-                <div class="user_img" style="background-image: url(\''.$row["img_path"].'\');" href="#" onclick="clickUserInAdminPage('.$id.'\')"></div>
-                <p>'.$user.'</p>
-                </li>';
-            }
-        }
-        return $result;
-    }
+    $persons = $functions->getAllFromTable("person");
+    $result = "<h2>No User found... :'(</h2>";
+    if($persons->num_rows >0){
+        $result = "";
+        while($row = $persons->fetch_assoc()){
+            //pass user if it is admin
+            if($row["is_admin"] == "1") continue;
+            $user = $row["firstname"].' '.$row["lastname"];
+            $id = $row["id"];
 
+            $result = $result.'<div class="column" style="background-color:#aaa000;">
+                        <div class="box1"><img style="width:120px;float:left" src="'.$row["img_path"].'" alt="user image"></div>
+                        <div class="box2">
+                            <table>
+                                <tr>
+                                  <td>Name</td>
+                                  <td>:</td>
+                                  <td>'.$user.'</td>
+                                </tr>
+                                <tr>
+                                 <td>Kontozustand</td>
+                                  <td>:</td>
+                                  <td>0 Euro</td>
+                                </tr>
+                                <tr>
+                                  <td>Letzte Getränk</td>
+                                  <td>:</td>
+                                  <td>Nix</td>
+                                </tr>
+                                </table>
+                                </div>
+                        <div class="box3">Box3</div>
+                    </div>';
+        }
+    }
+    return $result;
+}
 ?>
