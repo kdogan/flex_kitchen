be400817640e0667d2e73fc9a0ca11972db56daa
kdogan <kamuran1905@yahoo.de>
2018-01-11 22:36:45 +0100

    Improve admin page

--- admin.php
+++ admin.php
@@ -11,14 +11,27 @@ $functions->getSiteHead();
 echo '
 	<head>
 	<script>
-	function checkAccessForCurrentUser() {';
-	echo "if(".!isAdmin()."){
-		alert('Access danied for user id ".$_SESSION["userid"]."! You are not admin');
-		window.location.href = \"index.php\";}";
-echo '}
+	function checkAccessForCurrentUser() {
+		if('.!isAdmin().'){
+			alert("Access danied for this user! You are not admin");
+			window.location.href = "index.php";
+		}
+	}
+	function searchUserInAdminPage(){
+		
+      var value = document.getElementById("searchInAdminPage").value;
+      $("menu > div").each(function() {
+          if ($(this).text().toLowerCase().search(value.toLowerCase()) > -1) {
+              $(this).show();
+          }
+          else {
+              $(this).hide();
+          }
+      });
+	}
 </script>
 </head>
-<body onLoad="checkAccessForCurrentUser();">
+<body onLoad="checkAccessForCurrentUser()">
 
 <div class="flex-container">
 <header>
@@ -28,8 +41,9 @@ echo '}
 <div class="header_second_column" style="background-color:yellow;color:black"><h4> Aministration Bereich</h4></div>
 <div class="header_third_column">
   <form class="search_form">
-  <div style="float:right"><img style=" width:50px; float:right;" src="img/adminLogginImg.png" href="#" onclick="closeAdminSite()"></img></div>
-  <div style="float:right"><img style=" height:60px;float:right;" src="img/home.png" href="#" onclick="goToAdminHome()"></img></div>
+	<div style="float:right"><img style=" width:50px; float:right;" src="img/adminLogginImg.png" href="#" onclick="closeAdminSite()"></img></div>
+	<div style="float:right"><img style=" height:60px;float:right;" src="img/home.png" href="#" onclick="goToAdminHome()"></img></div>
+	<div style="float:right"> <input type="text" id="searchInAdminPage" onkeyup="searchUserInAdminPage()" placeholder="Search for names.." title="Type in a name"></div>
   </form>
 </div>
 </header>
