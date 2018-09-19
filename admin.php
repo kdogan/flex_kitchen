4868416bcc84cee7fc75cefd606acfaa0f206b16
kdogan <kamuran1905@yahoo.de>
2018-09-19 08:42:59 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -10,13 +10,13 @@ include ("add_customer_form.php");
 $functions = new functions();echo '
 <!DOCTYPE html>
 	<html>
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-			<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-			<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore.js"></script>
-			<script type="text/javascript" src="function.js"></script>
-			<script type="text/javascript" src="js/administration.js"></script>
-			<link rel="stylesheet" href="../css/style.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore.js"></script>
+		<script type="text/javascript" src="function.js"></script>
+		<script type="text/javascript" src="./js/administration.js"></script>
+                <link rel="stylesheet" href="./css/style.css">
 	<head>
 	<style>
 	.product_count {
